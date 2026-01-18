bb525288fa265be84b85a051957d0386c229c9db
Ian Ren <ianzren@outlook.com>
2026-01-18 12:30:39 +0000

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white font-sans">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+      </main>
+    </div>
+  );
+}
+
+
